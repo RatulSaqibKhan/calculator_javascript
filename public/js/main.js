5ef8caca3cd932b21e53e340b4cecf544be3e50f
Ratul Saqib Khan <ratulkhan.jhenidah@gmail.com>
2018-08-22 21:39:20 +0600

    Calculator App Develpment Mode

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,30 @@
 setTimeout(function () {
     $('body').addClass('loaded');
 }, 2000);
+window.onload = function () {
+    var buttons = document.getElementById('buttons');
+    var clear = document.getElementById('clear');
+    var answer = document.getElementById('answer');
+
+    buttons.addEventListener('click', function (e) {
+        if (e.target.nodeName === 'LI') {
+            value = e.target.innerHTML;
+            if (value === '=') {
+                try {
+                    answer.innerHTML = eval(answer.innerHTML);
+                } catch(e) {
+                    if (value === '=' || value === '.' || value === '/' || value === '*' || value === '+' || value === '-'){
+                        return;
+                    }
+                    answer.innerHTML = e.message;
+                }
+            } else {
+                answer.innerHTML += value;
+            }
+        }
+    });
+
+    clear.addEventListener('click', function () {
+        answer.innerHTML = '';
+    });
+};
